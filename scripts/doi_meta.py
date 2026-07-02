dff93ab5e4ae6f56ce7727819887157e57353eec
cailea <cailea@qq.com>
2026-07-02 10:01:45 +0800

    perf: 优化 doi 获取器

--- scripts/doi_meta.py
+++ scripts/doi_meta.py
@@ -13,10 +13,38 @@ def get_doi_metadata(doi):
     )
 
     with urllib.request.urlopen(req, timeout=20) as r:
-        msg = json.load(r)["message"]
+        msg = json.load(r)
+    
+    return msg["message"]
 
-    return msg
+def get_doi_abstractdata(doi):
+    msg = get_doi_metadata(doi)
+    return {
+        "source": msg["source"],
+        "language": msg["language"],
+        "title": msg["title"],
+        "original-title": msg["original-title"],
+        "short-title": msg["short-title"],
+        "subtitle": msg["subtitle"],
+        "author": msg["author"],
+        "DOI": msg["DOI"],
+        "URL": msg["URL"],
+        "is-referenced-by-count": msg["is-referenced-by-count"],
+        "reference-count": msg["reference-count"],
+        "publisher": msg["publisher"],
+        "container-title": msg["container-title"],
+        "short-container-title": msg["short-container-title"],
+        "type": msg["type"],
+        "volume": msg["volume"],
+        "page": msg["page"],
+        "issued": msg["issued"],
+        "article-number": msg["article-number"],
+        "ISSN": msg["ISSN"],
+        "funder": msg["funder"],
+        "reference": msg["reference"],   
+    }
 
 if __name__ == "__main__":
     doi = sys.argv[1]
-    print(json.dumps(get_doi_metadata(doi), ensure_ascii=False, indent=2))
+    
+    print(json.dumps(get_doi_abstractdata(doi), ensure_ascii=False, indent=2))
